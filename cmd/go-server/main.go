7ad9212aead442fd9fce51a39e1cead333ddbe9b
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-17 22:50:06 +0530

    load .env in go

--- cmd/go-server/main.go
+++ cmd/go-server/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"github.com/amreshpro/go-server/internal/router"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 func main() {
-	r := router.NewRouter()
+	r := http.NewServeMux()
+	port := os.Getenv("PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
 
+	fmt.Println("Server running on port:", port)
+	fmt.Println("DB User:", dbUser)
+	fmt.Println("DB Password:", dbPass)
 	log.Println("Starting server at :8080")
+	log.Println("AppName: "+os.Getenv("APP_NAME"))
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal("Server failed:", err)
-	}
+		}
+	
+	
 }
